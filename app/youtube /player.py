1250b9ea1523bc80f107b7ab2c83eedd1bdd063d
aashika1686 <aashika1686@gmail.com>
2026-09-09 09:25:42 +0530

    Update player.py

--- app/youtube /player.py
+++ app/youtube /player.py
@@ -2,77 +2,74 @@ import re
 import urllib.parse
 import urllib.request
 
+
 def get_vid(query):
 
-  try:
-    encoded = urllib.parse.quote(query)
-    
-    url = (
-      "https://www.youtube.com/results"
-      "?search_query=" + encoded
-    )
+    try:
+        encoded = urllib.parse.quote(query)
 
-    request = urllib.request.Request(
-      url,
-      headers = {
-          "user - Agent" : "Mozilla/5.0"
-      }
-    )
+        url = (
+            "https://www.youtube.com/results"
+            "?search_query=" + encoded
+        )
 
-    data = urllib.request.urlopen(
-        request,
-        timeout=5
-    ).read().decode("utf-8", errors="ignore")
+        request = urllib.request.Request(
+            url,
+            headers={
+                "User-Agent": "Mozilla/5.0"
+            }
+        )
 
-    ids = re.findall(
-      r'"videoId " : "([^"]+)"',
-      data
-    )
+        data = urllib.request.urlopen(
+            request,
+            timeout=5
+        ).read().decode("utf-8", errors="ignore")
 
-    return ids[0] if ids else None 
+        ids = re.findall(
+            r'"videoId":"([^"]+)"',
+            data
+        )
 
-  except Exception:
-      return None
-    
+        return ids[0] if ids else None
+
+    except Exception:
+        return None
+
+
 def create_youtube_url(command):
-  text = command.lower().strip()
 
-  patterns = [
-    r"play\s+song\s+(.+)",
-    r"play\s+music\s+(.+)",
-    r"play\s+(.+)"
-    r"youtube\s+(.+)
-  ]
+    text = command.lower().strip()
 
-  query = command
+    patterns = [
+        r"play\s+song\s+(.+)",
+        r"play\s+music\s+(.+)",
+        r"play\s+(.+)",
+        r"youtube\s+(.+)"
+    ]
 
-  for pattern in patterns:
+    query = command
 
-    match = re.search(
-      pattern,
-      text
-    )
-      
-    if match:
+    for pattern in patterns:
 
-      query = match.group(1)
-      break
+        match = re.search(
+            pattern,
+            text
+        )
+
+        if match:
+
+            query = match.group(1)
+            break
 
     query = query.strip()
-      
+
     video_id = get_vid(query)
 
     if not video_id:
-      return None
-      
-  return (
-    "https://www.youtube.com/embed/"
-    + video_id
-    +"?autoplay=1&mute=0"
-  )
+        return None
 
-
-
-
-
-
+    return (
+        "https://www.youtube.com/embed/"
+        + video_id
+        + "?autoplay=1&mute=0"
+    )
